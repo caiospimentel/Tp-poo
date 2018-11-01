a7fd2df580062a353a1accc14b6adcdbe701dde8
Unknown <caio3191@gmail.com>
2018-11-01 09:45:04 -0300

    Testes iniciais da implementação das classes

---
+++ Parte 1/base.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+
+class Edge{
+int v1, v2;
+int w;
+public:
+  Edge(int a, int b, int c){
+    v1 = a;
+    v2 = b;
+    w = c;
+  }
+  vector<int> listEdges(){
+    return vector<int> v[a,b];
+  }
+  
+
+};
+
+
+class Graph{
+
+
+
+};
