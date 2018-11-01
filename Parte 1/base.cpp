4a011a4e2f99de7eb7f99c06f259722792c58805
Unknown <caio3191@gmail.com>
2018-11-01 17:09:05 -0300

    OBG ARIEL

--- Parte 1/base.cpp
+++ Parte 1/base.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-class Edge{
+class Edge{ // classe das arestas do grafo
 int v1, v2;
 int w;
 public:
@@ -12,27 +12,53 @@ public:
     v2 = b;
     w = c;
   }
-  vector<int> showPoints(){
-    return vector<int> v[2] = [v1,v2];
+  vector<int> getPoints(){ // função de interfaceamento dos vértices da aresta
+    return vector<int> v[2] = [v1,v2]; //** não sei se vai dar certo **
   }
-  int showWeight(){
-    return int a = w;
+  int getWeight(){// Função de interfaceamento do peso da aresta
+    return w;
   }
 
 };
 
-class Graph{
+class Graph{ // Classe do grafo
   int vertices;
   int edges;
   vector<vector<int>> adjMatrix;
 public:
-  Graph(int v){
+  Graph(int v){ //Construtor
     vertices = v;
     v.resize(vertices, vector<float>(vertices,0));
   }
-  ~Graph(){
+
+  ~Graph(){//Destrutor
     adjMatrix.clear();
   }
 
+  bool insert(const Edge& a){ // não sei se a passagem por referencia deixa chamar métodos dessa forma
+    vector<int> v = a.getPoints(); //Não sei se vai dar certo
+    //TODO: tratar fora do range o valor do vértice
+    int w = a.getWeight();
+    if(adjMatrix[v[0],v[1]]==0){ //caso a aresta exista, mas com peso diferente, altera o peso
+      adjMatrix[v[0],v[1]] = w;
+      adjMatrix[v[1],v[0]] = w;
+      edges++;
+      return true;
+    }
+    else
+      return false;
+  }
 
-};
+  bool remove(const Edge& a){
+    vector<int> v = a.getPoints(); //Não sei se vai dar certo
+    int w = a.getWeight();
+    if(adjMatrix[v[0],v[1]]!=0){
+
+    }
+
+
+  }
+
+
+
+  };
